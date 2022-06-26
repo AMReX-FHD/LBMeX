d53e47df8ffe6b498b83682e84f6fd5412412c82
Ulf Schiller <uschill@clemson.edu>
2022-06-25 19:02:28 -0700

    Initial stubs for binary LBM

---
+++ GuidedTutorials/LBM/binary/main_driver.cpp
@@ -0,0 +1,123 @@
+#include <AMReX.H>
+#include <AMReX_MultiFab.H>
+#include <AMReX_MFParallelFor.H>
+#include <AMReX_Print.H>
+#include <AMReX_ParmParse.H>
+#include <AMReX_PlotFileUtil.H>
+
+using namespace amrex;
+
+#include "LBM_binary.H"
+
+inline void WriteOutput(int step,
+			const MultiFab& hydrovs,
+			const Vector<std::string>& var_names,
+			const Geometry& geom) {
+  const Real time = step;
+  const std::string& pltfile = amrex::Concatenate("plt",step,5);
+  WriteSingleLevelPlotfile(pltfile, hydrovs, var_names, geom, time, step);
+}
+
+inline Vector<std::string> VariableNames(const int numVars) {
+  // set variable names for output
+  Vector<std::string> var_names(numVars);
+  std::string name;
+  int cnt = 0;
+  // rho, phi, psi
+  var_names[cnt++] = "rho";
+  var_names[cnt++] = "phi";
+  // velx, vely, velz
+  for (int d=0; d<AMREX_SPACEDIM; d++) {
+    name = "u";
+    name += (120+d);
+    var_names[cnt++] = name;
+  }
+  var_names[cnt++] = "p_bulk";
+  // pxx, pxy, pxz, pyy, pyz, pzz
+  for (int i=0; i<AMREX_SPACEDIM, cnt<numVars; ++i) {
+    for (int j=i; j<AMREX_SPACEDIM, cnt<numVars; ++j) {
+      name = "p";
+      name += (120+i);
+      name += (120+j);
+      var_names[cnt++] = name;
+    }
+  }
+  // kinetic moments
+  for (; cnt<numVars;) {
+    name = "m";
+    name += std::to_string(cnt);
+    var_names[cnt++] = name;
+  }
+  return var_names;
+}
+
+void main_driver(const char* argv) {
+
+  // default grid parameters
+  int nx = 16;
+  int max_grid_size = 8;
+
+  // default time stepping parameters
+  int nsteps = 100;
+  int plot_int = 10;
+
+  // default droplet radius (% of box size)
+  Real radius = 0.3;
+
+  // input parameters
+  ParmParse pp;
+  pp.query("nx", nx);
+  pp.query("max_grid_size", max_grid_size);
+  pp.query("nsteps", nsteps);
+  pp.query("plot_int", plot_int);
+
+  // set up Box and Geomtry
+  IntVect dom_lo(0, 0, 0);
+  IntVect dom_hi(nx-1, nx-1, nx-1);
+  Array<int,3> periodicity({1,1,1});
+
+  Box domain(dom_lo, dom_hi);
+
+  RealBox real_box({0.,0.,0.},{1.,1.,1.});
+  
+  Geometry geom(domain, real_box, CoordSys::cartesian, periodicity);
+
+  BoxArray ba(domain);
+
+  // split BoxArray into chunks no larger than "max_grid_size" along a direction
+  ba.maxSize(max_grid_size);
+
+  DistributionMapping dm(ba);
+
+  // need two halo layers for gradients
+  int nghost = 2;
+
+  // number of hydrodynamic fields to output
+  int nhydro = 6;
+
+  // set up MultiFabs
+  MultiFab fold(ba, dm, nvel, nghost);
+  MultiFab fnew(ba, dm, nvel, nghost);
+  MultiFab gold(ba, dm, nvel, nghost);
+  MultiFab gnew(ba, dm, nvel, nghost);
+  MultiFab hydrovs(ba, dm, nhydro, nghost);
+
+  // set up variable names for output
+  const Vector<std::string> var_names = VariableNames(nhydro);
+
+  // INITIALIZE
+  LBM_init_mixture(fold, gold, hydrovs);
+  // Write a plotfile of the initial data if plot_int > 0
+  if (plot_int > 0)
+    WriteOutput(0, hydrovs, var_names, geom);
+  Print() << "LB initialized\n";
+
+  // TIMESTEP
+  for (int step=1; step <= nsteps; ++step) {
+    LBM_timestep(geom, fold, gold, fnew, gnew, hydrovs);
+    if (plot_int > 0 && step%plot_int ==0)
+      WriteOutput(step, hydrovs, var_names, geom);
+    Print() << "LB step " << step << "\n";
+  }
+
+}
